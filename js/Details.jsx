123f54f2fe14e5bc3476cf5619b2e59b5b1f7c71
James Gosbell <jamesgosbell@gmail.com>
2016-11-27 11:31:59 +1100

    Added onEnter route for show details and changed details to a es6 component

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,13 +1,21 @@
 const React = require('react')
+const { object } = React.PropTypes
 
-class Details extends React.Component {
+const Details = React.createClass({
+  propTypes: {
+    params: object.isRequired
+  },
   render () {
     return (
       <div className='container'>
-
+        <pre style={{textAlign: 'left'}} >
+          <code>
+            {JSON.stringify(this.props.params, null, 4)}
+          </code>
+        </pre>
       </div>
     )
   }
-}
+})
 
 module.exports = Details

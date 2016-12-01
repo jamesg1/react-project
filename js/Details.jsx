0ee775def6656ff88d85ab176875536f823c71c5
James Gosbell <jamesgosbell@gmail.com>
2016-12-01 13:05:06 +0700

    Added omdb and local api ajax call with axios for video rating

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,5 +1,6 @@
 const React = require('react')
 const Header = require('./Header')
+const axios = require('axios')
 const { connector } = require('./Store')
 const { arrayOf, object } = React.PropTypes
 
@@ -8,18 +9,40 @@ const Details = React.createClass({
     params: object,
     shows: arrayOf(object).isRequired
   },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    // for online api = `http://www.omdbapi.com/?i=${this.assignShow[this.props.params.id].imdbID}`
+    axios.get('../public/api.json')
+    .then((response) => {
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => {
+      console.error('axios error', error)
+    })
+  },
   assignShow (id) {
     const showArray = this.props.shows.filter((show) => show.imdbID === id)
     return showArray[0]
   },
   render () {
     const { title, description, year, poster, trailer } = this.assignShow(this.props.params.id)
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3 className='video-rating'>
+        {this.state.omdbData.imdbRating}
+      </h3>
+    }
     return (
       <div className='container'>
         <Header />
         <div className='video-info'>
           <h1 className='video-title'>{title}</h1>
           <h2 className='video-year'>({year})</h2>
+          {rating}
           <img className='video-poster' src={`/public/img/posters/${poster}`} />
           <p className='video-description'>{description}</p>
         </div>
